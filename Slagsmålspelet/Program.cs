49ba8866aba57ea87c8b2a61b5dcb3de3e4d78f4
Maximilian Persson <Maximilian.persson2@elev.ga.ntig.se>
2022-10-13 15:11:40 +0200

    13/10/2022

---
+++ Slagsmålspelet/Program.cs
@@ -0,0 +1,46 @@
+﻿Random generator = new Random();
+
+int dudeHp= 50;
+int boyHp= 50;
+int dudeDamage;
+int boyDamage;
+
+
+
+Console.WriteLine(dudeHp> 0 && boyHp > 0);
+
+
+string dude ="dude";
+string boy  = "boy";
+
+while (dudeHp > 0 && boyHp > 0)
+{
+    Console.WriteLine($"dude: {dudeHp} boy {boyHp}:\n");
+    boyDamage = generator.Next(10);
+    dudeHp -= boyDamage;
+    Console.WriteLine($"boy deals {boyDamage} on {dudeHp} ");
+
+    dudeDamage = generator.Next(10);
+    boyHp = dudeDamage;
+    Console.WriteLine($" dude deals {dudeDamage} on {boyHp}  ");
+    break;
+
+
+
+    Console.ReadKey();
+}
+
+if (dudeHp == 0 && boyHp == 0)
+{
+   Console.WriteLine("You both suck!");
+}
+else if (boyHp == 0)
+{
+    Console.WriteLine("The dude won!");
+}
+else if (dudeHp == 0)
+{
+    Console.WriteLine("Good boy you won!");
+}
+
+Console.ReadLine();
